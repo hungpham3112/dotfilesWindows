746d4a22f2215b29c0848941dd90c9f56f6c83f6
hungpham3112 <75968004+hungpham3112@users.noreply.github.com>
2022-11-20 09:38:38 +0700

    add powershell $PROFILE settings

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,1 @@
+. $HOME\\.config\\powershell\\user_profile.ps1
---
+++ PowerShell/profile.ps1
@@ -0,0 +1,6 @@
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "C:\Users\admin\scoop\apps\miniconda3\current\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
+
