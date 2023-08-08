91ae9d2d5364f36f11ba80461934d8c07f6063d6
Hung Pham <phamhung20022015@gmail.com>
2023-08-09 01:34:12 +0700

    feat(winfetch): random choose image from folder

--- winfetch/config.ps1
+++ winfetch/config.ps1
@@ -1,15 +1,40 @@
 # ===== WINFETCH CONFIGURATION =====
 
- #$image = "$env:USERPROFILE/.config/winfetch/image.png"
- $logo = "Windows 10"
- $ShowDisks = @("C:")
- $ShowPkgs = @("scoop")
- function info_custom_time {
+$currentDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$imagesFolder = Join-Path -Path $currentDir -ChildPath "images"
+
+# Array of image file extensions to filter
+$imageExtensions = @(".jpg", ".jpeg", ".png", ".gif")
+
+# Function to recursively find image file paths
+function Get-ImageFilePaths {
+    param (
+        [string]$folder,
+        [string[]]$extensions
+    )
+    
+    Get-ChildItem -Path $folder -Recurse | Where-Object {
+        $_.Extension -in $extensions
+    } | Select-Object -ExpandProperty FullName
+}
+
+# Get image file paths
+$imageFilePaths = Get-ImageFilePaths -folder $imagesFolder -extensions $imageExtensions
+
+# Output the list of image file paths
+$image = Get-Random $imageFilePaths
+
+# Rest of your WinFetch configuration here...
+$logo = "Windows 10"
+$ShowDisks = @("C:")
+$ShowPkgs = @("scoop")
+function info_custom_time {
     return @{
         title = "Time"
         content = (Get-Date)
     }
 }
+
 @(
     "title"
     "dashes"
